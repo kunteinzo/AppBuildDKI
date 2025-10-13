8a35d0813b66cb846c2d40b7015f31d3dbe953eb
Kunteinzo <theinhtetoo473@gmail.com>
2025-10-13 13:08:00 +0300

    up

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ COPY *.py Api/
 COPY *.sh .
 
 RUN apt-get update
-RUN apt-get install openjdk-21-jdk wget unzip python3 python3-venv python3-pip -y
+RUN apt-get install openjdk-21-jdk wget unzip python3 python3-venv python3-pip ssh -y
 RUN python3 -m venv .venv
 RUN .venv/bin/pip install -U pip 'fastapi[standard]' pyjwt python-multipart
 RUN mkdir -p Android/Sdk/cmdline-tools/latest
--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI, Depends, HTTPException, File, UploadFile
+from fastapi.responses import FileResponse
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
 import jwt
+import subprocess
+import os
+
+HOME = os.getenv("HOME")
+PJ_DIR = HOME+"/PJ"
+APK_PATH = "/app/build/outputs/apk/"
+
+def check():
+    if not os.path.exists(PJ_DIR):
+        os.mkdir(PJ_DIR)
+
+
+def cmd(cmd: list|str):
+    return subprocess.run(
+        cmd,
+        capture_output=True, text=True
+    )
+
 
 app = FastAPI()
 oauth = OAuth2PasswordBearer(tokenUrl="token")
@@ -20,11 +39,11 @@ async def ck(token: Annotated[str, Depends(oauth)]):
 
 
 @app.get("/thekey")
-def index():
+def index(is_dev: int = 0):
     return dict(token=jwt.encode(
         dict(
             uid=1000,
-            exp=datetime.now(timezone.utc) + timedelta(minutes=2)
+            exp=datetime.now(timezone.utc) + timedelta(minutes=(60 if is_dev == 1 else 2))
             ),
         "secret",
         algorithm="HS512"
@@ -34,3 +53,18 @@ def index():
 @app.post("/build")
 def build(type: str, file: UploadFile, token: Annotated[dict, Depends(ck)]):
     return dict(msg=type, token=token, file_name=file.filename)
+
+
+@app.post("/uploadBuild")
+def upload_build(file: UploadFile, token: Annotated[dict, Depends(ck)]):
+    check()
+    with open(f"{PJ_DIR}/{file.filename}", "wb") as f:
+        while len(b:=file.file.read(1024)) > 0:
+            f.write(b)
+    res = cmd(["unzip", "-o", "-K", f"{HOME}/{file.filename}", "-d", f"{PJ_DIR}"])
+    PJD = f"{PJ_DIR}/{file.filename[:-4]}"
+    cmd(["chmod", "+x", f"{PJD}/gradlew"])
+    #cmd(["cd", PJD, "&&", "./gradlew", "assemble"])
+    zip_name = f"{file.filename[:-4]}.zip",
+    cmd(["cd", PJD, "&&", "zip", "-r", zip_name, "*"])
+    return FileResponse(PJD+'/'+zip_name)
