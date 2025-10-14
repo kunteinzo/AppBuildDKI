6abefcdad91968c4e64dbd3b6e01fae02caabe9e
Kunteinzo <theinhtetoo473@gmail.com>
2025-10-14 11:27:02 +0300

    up

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ $PWD != "$HOME/Api" ];then cd $HOME/Api;fi
 
-sdkmanager --licenses
+/root/Android/Sdk/cmdline-tools/latest/bin/sdkmanager --licenses
 
 source $HOME/.venv/bin/activate
 fastapi dev --host 0.0.0.0
